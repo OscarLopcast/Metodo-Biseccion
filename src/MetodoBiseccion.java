f0dfeee818fe70f1df1d3b819bad900938faba84
Oscar Fernando López Castro <Oscar.fer.22.22@hotmail.com>
2025-05-08 09:36:19 -0700

    creacion de la logica del metodo

--- src/MetodoBiseccion.java
+++ src/MetodoBiseccion.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class MetodoBiseccion {
-	public static void main(String[] args) {
-		System.out.println("Método de Bisección en Java");
-		System.out.println("prueba1");
-	}
+
+	//metodo funcion
+    public static double funcion(double x) {
+        return x * x - 2;
+    }
+
+	//metodo biseccion
+    public static double biseccion(double a, double b, double tolerancia) {
+        double c = a;
+        while ((b - a) >= tolerancia) {
+            c = (a + b) / 2;
+            if (funcion(c) == 0.0)
+                break;
+            else if (funcion(c) * funcion(a) < 0)
+                b = c;
+            else
+                a = c;
+        }
+        return c;
+    }
 }
